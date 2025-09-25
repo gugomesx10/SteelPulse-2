c5d3d4a52ff27a4bf3a940137a2f6e41209edf86
gugomesx10 <gustavogomesmartins4@icloud.com>
2025-09-25 14:03:16 -0300

    Criando components

--- Steel-Pulse/src/App.tsx
+++ Steel-Pulse/src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Banner from "./components/Banner/Banner";
+import Link from "./components/Link/Link";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Banner src="./banner.png" alt="Banner"/>;
+      <Link href="/" target="_blank">Link</Link>
     </>
   )
 }
 
-export default App
+export default App;
